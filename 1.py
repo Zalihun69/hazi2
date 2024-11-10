afadbea26f5880fc4949f4230cc36a36262a551f
Zalihun69 <159771111+Zalihun69@users.noreply.github.com>
2024-11-10 18:32:27 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,15 @@
+
+
+keresztnevek = []
+
+
+keresztnev = None
+while keresztnev != '':
+    keresztnev = input('Adj meg egy kersztnevet! ')
+    if keresztnev != '':
+
+        keresztnevek.append(keresztnev)
+
+print(keresztnevek)
+
+
